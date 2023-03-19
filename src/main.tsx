ed1b8bc14a82c2aebab0fd4f9a94dc52a86e8787
SongNoin <as5427072@gmail.com>
2023-03-20 00:54:36 +0900

    style 글로벌 스타일 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import GlobalStyle from './styles/GlobalStyle';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />
+  <>
+    <GlobalStyle />
+    <App />
+  </>
 );
