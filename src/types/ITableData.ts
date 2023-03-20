45479ec93fc12c29b2d891074c218e7477d4e13b
SongNoin <as5427072@gmail.com>
2023-03-20 18:16:23 +0900

    fix table Data 타입 수정

--- src/types/ITableData.ts
+++ src/types/ITableData.ts
@@ -2,6 +2,7 @@ export interface ITableData {
   id: number;
   transaction_time: string;
   status: boolean;
+  statusText?: '완료' | '미완료';
   customer_id: number;
   customer_name: number;
   currency: string;
