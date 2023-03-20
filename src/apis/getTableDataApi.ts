7b2673a0df71a0356f37b1d733283b75633a419f
SongNoin <as5427072@gmail.com>
2023-03-20 18:58:52 +0900

    fix table data api 날짜 필터 추가

--- src/apis/getTableDataApi.ts
+++ src/apis/getTableDataApi.ts
@@ -1,10 +1,22 @@
 import axios from 'axios';
+import { ITableData } from '../types/ITableData';
 
-export const getTableDataApi = async () => {
+export const getTableDataApi = async (date?: string) => {
   try {
     const chartRes = await axios.get('/data/table_data.json');
     if (chartRes.statusText === 'OK') {
-      const tableData = chartRes.data;
+      let tableData: ITableData[] = chartRes.data;
+      if (date) {
+        tableData = tableData.filter(
+          (el) => el.transaction_time.split(' ')[0] === date
+        );
+      }
+
+      for (let i = 0; i < tableData.length; i++) {
+        tableData[i].status
+          ? (tableData[i].statusText = '완료')
+          : (tableData[i].statusText = '미완료');
+      }
 
       return tableData;
     }
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,18 +1,23 @@
 import { useEffect, useState } from 'react';
 import { getTableDataApi } from '../apis/getTableDataApi';
+import { MainTable } from '../components/table/MainTable';
 import { ITableData } from '../types/ITableData';
 
 export const MainPage = () => {
-  const [tableData, setTableData] = useState<ITableData[]>();
+  const [tableData, setTableData] = useState<ITableData[]>([]);
+  const todayDate = '2023-03-08';
 
   useEffect(() => {
     getTableData();
   }, []);
 
   async function getTableData() {
-    const tableResData = await getTableDataApi();
-    setTableData(tableResData);
+    const tableResData = await getTableDataApi(todayDate);
+    tableResData && setTableData(tableResData);
   }
-  console.log(tableData);
-  return <></>;
+  return (
+    <>
+      <MainTable tableData={tableData} />
+    </>
+  );
 };
